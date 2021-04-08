0f50e2a460c7140dcbdadea9fca18ec090f0c5ac
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-04-08 08:52:14 +0200

    ✅ Test exceptions when parsing rules

--- tests/testthat/test-get_rules.R
+++ tests/testthat/test-get_rules.R
@@ -1,3 +1,14 @@
-test_that("Error getting rules if location is not specified", {
-    expect_error(get_rules("foo"), regexp = "There are no JSON files with rules in")
+test_that("Error when there is no rules folder", {
+    expect_error(get_rules("foo"), regexp = "foo does not have a 'rules' subfolder")
+})
+
+
+test_that("Error when there are no JSON files with rules", {
+    fake_sysreq_folder <- file.path(tempdir(), "sysreq")
+    fake_rules_folder <- file.path(fake_sysreq_folder, "rules")
+
+    dir.create(fake_rules_folder, recursive = TRUE)
+    on.exit(unlink(fake_rules_folder))
+
+    expect_error(get_rules(fake_sysreq_folder), regexp = "There are no JSON files with rules in")
 })
