61207152c8b6aba1f7d90836e75c1160e623aa7d
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-06-23 22:55:47 +0200

    ✅ Test parsing of rules

--- tests/testthat/test-parse_rules.R
+++ tests/testthat/test-parse_rules.R
@@ -5,10 +5,33 @@ test_that("Error when there is no rules folder", {
 
 test_that("Error when there are no JSON files with rules", {
     fake_sysreq_folder <- file.path(tempdir(), "sysreq")
-    fake_rules_folder <- file.path(fake_sysreq_folder, "rules")
 
-    dir.create(fake_rules_folder, recursive = TRUE)
-    on.exit(unlink(fake_rules_folder))
+    dir.create(file.path(fake_sysreq_folder, "rules"), recursive = TRUE)
+    on.exit(unlink(fake_sysreq_folder, recursive = TRUE))
 
     expect_error(get_rules(fake_sysreq_folder), regexp = "There are no JSON files with rules in")
+})
+
+
+test_that("Parse (subset) of rules", {
+    system_req_folder <- file.path(tempdir(), "pkg.deps_test_rules")
+    on.exit(unlink(system_req_folder, recursive = TRUE))
+
+    # rules.zip contains the files r-system-requirements/rules/z*.json
+    rules_archive <- testthat::test_path("test-files", "rules.zip")
+    unzip(rules_archive, exdir = system_req_folder)
+
+    rules <- parse_rules(system_req_folder)
+
+    expect_named(rules, c("Rule", "Package", "OS", "Distribution", "Patterns", "CollapsedPatterns"))
+    expect_gt(nrow(rules), 0)
+
+    expect_true(
+        all(c("ubuntu", "debian", "centos", "redhat", "opensuse", "sle") %in% rules$Distribution)
+    )
+
+    expect_true(all(c("linux", "windows") %in% rules$OS))
+
+    windows_rules <- subset(rules, OS == "windows")
+    expect_true(all(is.na(windows_rules$Distribution)))
 })
